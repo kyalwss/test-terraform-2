2466384519b788837436254ffe16e81e3842b314
kyalwss <87778734+kyalwss@users.noreply.github.com>
2023-02-20 10:47:08 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
 terraform {
   required_version = ">= 1.3.8, < 2.0.0"
-  backend "s3" {
-    key    = "my-state.tfstate"
-    region = "us-east-1"
-  }
-  required_providers {
-    aws = "< 5.0.0"
-  }
 }
 
 provider "aws" {
